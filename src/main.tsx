daae0fd726a85f392c815ce75daec6a4564a0c9a
wood.wu <kilokingw@gmail.com>
2025-07-29 16:30:59 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,22 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* 暫時隱藏網站 */}
+    <div
+      style={{
+        backgroundColor: "black",
+        width: "100vw",
+        height: "100vh",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        zIndex: 9999,
+      }}
+    ></div>
+
+    {/* 原本的網站內容 */}
+    {/* <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
   </StrictMode>
 );
